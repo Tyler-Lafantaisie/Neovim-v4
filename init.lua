4b3d37ace933c3821ee8fa3fe083417bf5c1a56a
Tyler Lafantaisie <tyler@localhost.localdomain>
2024-08-18 18:43:45 -0400

    first commit

---
+++ init.lua
@@ -0,0 +1,15 @@
+-- numberline
+vim.opt.nu = true
+vim.opt.relativenumber = true
+--tabspacing
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+--other settings
+vim.opt.termguicolors = true
+vim.g.mapleader = " "
+
+-- plugin manager 
+require("config.lazy")
